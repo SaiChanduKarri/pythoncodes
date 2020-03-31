c3324a34551a629ed52822b296be707253336f13
SaiChanduKarri <56600025+SaiChanduKarri@users.noreply.github.com>
2020-03-31 13:15:36 +0530

    Add files via upload

---
+++ sortedstring.py
@@ -0,0 +1,7 @@
+a = input()
+b = sorted(a)
+print(b)
+for i in b:
+    print(i,end='')
+
+    # Two methods
--- sum.py
+++ sum.py
@@ -1,5 +1,12 @@
-
-fact = int(input())
-for i in range(1,fact):
-    fact = fact * i
-print(fact)
+a = int(input("enter a value:"))
+b = int(input("enter a value:"))
+l = []
+l.extend((a,b))
+print(l)
+li = []
+li.append(a)
+li.append(b)
+print(li)
+print( sum(li) )
+print("the sum is:",sum(l))
+ # Two methods
